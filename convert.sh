88513d9d6315034c1281eaa823fdea0293556bf8
James Badger <james@jamesbadger.ca>
2019-03-28 11:18:32 -0600

    Add project-clip-project output

--- convert.sh
+++ convert.sh
@@ -3,30 +3,38 @@
 NAME="simplified_land_polygons"
 ORIGINAL="data/$NAME/$NAME.shp"
 
+# 1) Input file
+# 2) Output directory
+# 3) Projection ID
+function transform {
+	mkdir -p "$2"
+	filename=$(basename "$1")
+	if [ ! -f "$2/$filename" ]; then
+		ogr2ogr -t_srs "$3" "$2/$filename" $1
+	fi
+}
+
+# 1) Input file
+# 2) Output directory
+function clip {
+	mkdir -p "$2"
+	filename=$(basename "$1")
+	if [ ! -f "$2/$filename" ]; then
+		ogr2ogr -clipsrc -180 40 0 90 "$2/$filename" $1
+	fi
+}
+
 # convert 3857 to 4326
-DIR1="data/4326"
-if [ ! -f $DIR1/$NAME.shp ]; then
-	mkdir $DIR1
-	ogr2ogr -t_srs "EPSG:4326" $DIR1/$NAME.shp $ORIGINAL
-fi
+transform $ORIGINAL "data/4326" "EPSG:4326"
 
 # convert 4326 to 3573
-DIR2="data/4326-3573"
-if [ ! -f $DIR2/$NAME.shp ]; then
-	mkdir $DIR2
-	ogr2ogr -t_srs "EPSG:3573" $DIR2/$NAME.shp $DIR1/$NAME.shp
-fi
+transform "data/4326/$NAME.shp" "data/4326-3573" "EPSG:3573"
 
 # convert 3857 to 3573
-DIR3="data/3573"
-if [ ! -f $DIR3/$NAME.shp ]; then
-	mkdir $DIR3
-	ogr2ogr -t_srs "EPSG:3573" $DIR3/$NAME.shp $ORIGINAL
-fi
+transform $ORIGINAL "data/3573" "EPSG:3573"
 
 # clip 4326
-DIR4="data/4326-clip"
-if [ ! -f $DIR4/$NAME.shp ]; then
-	mkdir $DIR4
-	ogr2ogr -clipsrc -180 40 0 90 $DIR4/$NAME.shp $DIR1/$NAME.shp
-fi
+clip "data/4326/$NAME.shp" "data/4326-clipped"
+
+# convert 4326-clipped to 3573
+transform "data/4326-clipped/$NAME.shp" "data/4326-clipped-3573" "EPSG:3573"
